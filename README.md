423f8f1939b05038ccdc5c1bcd7fd0beca8a1571
licai <1036583615@qq.com>
2021-07-09 17:52:58 +0800

    feat: 完成雏形

---
+++ README.md
@@ -0,0 +1,19 @@
+# js-utils
+
+# 一、简介
+结合 [imondo/js-utils](https://github.com/imondo/js-utils) 和 [nhn/tui.jsdoc-template](https://github.com/nhn/tui.jsdoc-template)
+实现的js工具库（常用函数）文档管理工具。采用的是jsdoc+docdash主题。
+
+# 二、功能
+1、支持jsdoc注释自动生成文档
+
+2、支持热更新(hot update) /实时更新(live update)
+
+# 三、使用方法
+1、使用`gulp serve`或者`npm run dev` 指令启动服务 （会在项目根目录生成docs目录）
+
+2、使用`npm run build` 打包项目（会在项目根目录生成docs目录）
+
+# 四、待实现
+支持发布npm包
+
---
+++ gulpfile.js
@@ -0,0 +1,101 @@
+/**
+ * @author NHN Ent. FE Development Team <dl_javascript@nhnent.com>
+ * @fileoverview Gulpfile
+ */
+'use strict';
+var path = require('path');
+var gulp = require('gulp');
+var del = require('del');
+var connect = require('gulp-connect');
+var jsdoc = require('gulp-jsdoc3');
+
+/* Demo paths */
+var SRC_DIR_PATH = path.join(__dirname, 'src');
+var SRC_DIR_TEMPLATE_PATH = __dirname;
+var SRC_DIR_DESTINATION_PATH = 'hotupdate';
+
+var srcFilePaths = ['src/**/*.js'].map(function (filePath) {
+    return path.join(SRC_DIR_PATH, filePath);
+});
+srcFilePaths.push('README.md');
+
+gulp.task('src:default', ['del'], function(done) {
+    var config = {
+        opts: {destination: path.join(SRC_DIR_DESTINATION_PATH, '-docstrap')}
+    };
+
+    gulp.src(srcFilePaths, {read: false})
+        .pipe(jsdoc(config, done));
+});
+
+/**
+ * Generate src document
+ */
+gulp.task('src', ['del'], function(done) {
+    /* Demo config */
+    var domeConfigPath = path.join(__dirname,'jsdoc.json');
+    console.log('domeConfigPath======',domeConfigPath)
+    var config = require(domeConfigPath);
+    delete require.cache[require.resolve(domeConfigPath)]; // remove cache
+
+    // config.opts.template = SRC_DIR_TEMPLATE_PATH;
+    config.opts.destination = SRC_DIR_DESTINATION_PATH;
+
+    gulp.src(srcFilePaths, {read: false})
+        .pipe(jsdoc(config, done));
+});
+
+/**
+ * Watch file paths
+ * @type {string[]}
+ */
+var watchPaths = [
+    'src/**/*.js',
+    'jsdoc.json',
+    'plugins/**/*.tmpl',
+    'plugins/**/*.js',
+];
+
+/**
+ * Reload server
+ */
+gulp.task('reload', ['src'], function() {
+    return gulp.src(watchPaths)
+        .pipe(connect.reload())
+});
+
+/**
+ * Regenerate src document when a file changes
+ */
+gulp.task('watch', ['src'] ,function() {
+    var watcher = gulp.watch(watchPaths, ['src', 'reload']);
+
+    watcher.on('change', function (event) {
+        console.log('File: ' + event.path + ' was ' + event.type + ', running tasks...');
+    });
+});
+
+/**
+ * Run web server
+ */
+gulp.task('connect', ['src'], function() {
+    connect.server({
+        root: SRC_DIR_DESTINATION_PATH,
+        livereload: true
+    });
+});
+
+/**
+ * @command gulp serve
+ * Connect-server with watch
+ */
+gulp.task('serve', ['connect', 'watch']);
+
+
+/**
+ * @command gulp del
+ * Delete all src-doc files
+ */
+gulp.task('del', function() {
+    return del([SRC_DIR_DESTINATION_PATH]);
+});
