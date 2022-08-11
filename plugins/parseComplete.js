64429d78f659452ce0b09b1e9188aafaa2b4ef18
licai <1036583615@qq.com>
2022-08-11 17:47:49 +0800

    docs: 完善说明文档

--- plugins/parseComplete.js
+++ plugins/parseComplete.js
@@ -9,7 +9,7 @@ exports.handlers = {
         // console.log('exec', exec)
         const originFilesAndDir=`${process.cwd()}/src`;
         const hotupdateFilesAndDir=`${process.cwd()}/hotupdate/src`;
-        const docsFilesAndDir=`${process.cwd()}/docs/src`;
+        // const docsFilesAndDir=`${process.cwd()}/docs/src`;
         fse.copy(originFilesAndDir, hotupdateFilesAndDir)
             .then(() => console.log(`从${originFilesAndDir}复制至${hotupdateFilesAndDir}成功！`))
             .catch(err => console.error(`从${originFilesAndDir}复制至${hotupdateFilesAndDir}失败！，原因为${err}`))
