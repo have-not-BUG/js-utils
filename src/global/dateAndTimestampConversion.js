432ee863769c5f1d5a4f479635295d57a14a9598
licai <1036583615@qq.com>
2022-08-11 10:10:37 +0800

    feat: 完善时间戳及日期相互转换函数及单元测试

--- src/global/dateAndTimestampConversion.js
+++ src/global/dateAndTimestampConversion.js
@@ -10,11 +10,22 @@
  * dateAndTimestampConversion('2022/08/10 16:37:51') // 1660120671000
  */
  function dateAndTimestampConversion(time, isDisplayDate = true) {
+     const timeType= Object.prototype.toString.call(time)
+    if(timeType !=='[object Number]' && timeType !=='[object String]'){
+        throw new Error("参数应为数字或字符串类型")
+    }
+    const isTimeTypeIsNum= timeType ==='[object Number]';
+    if(isTimeTypeIsNum && String(time).length !==10 && String(time).length !==13){
+        throw new Error("时间戳位数应为11位数或8位数")
+    }
+    if(!isTimeTypeIsNum && time.length !==10 && time.length !==19){
+        throw new Error("日期位数应为10位数或19位数")
+    }
     if (String(time).includes('-') || String(time).includes('/')) {
         const timeTransition=time.replace(/[-]/g,'/')
         return new Date(timeTransition).getTime()
     } else {
-        const now = new Date(String(time).length < 11 ? Number(time) * 1000 : Number(time))
+        const now = new Date(String(time).length ===10 ? Number(time) * 1000 : Number(time))
         const year = now.getFullYear()
         const month = (now.getMonth() + 1).toString().padStart(2, '0')
         const date = now.getDate().toString().padStart(2, '0')
