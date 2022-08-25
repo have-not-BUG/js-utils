6f6f5ae27bb99bede4a877c542a36d31f872d5f6
licai <1036583615@qq.com>
2022-08-25 17:37:10 +0800

    fix: 1、完善toFixed、formatcurrency方法及单元测试

--- src/__tests__/formatCurrency.spec.js
+++ src/__tests__/formatCurrency.spec.js
@@ -4,12 +4,18 @@ describe('formatCurrency 方法测试', () => {
 
 
 
-  test('测试保留两位小数', () => {
+  test('测试保留两位小数-正数', () => {
     expect(formatCurrency(12356.12454,'$',2)).toBe('$12,356.12')
     expect(formatCurrency(12356.105,'$',2)).toBe('$12,356.11')
     expect(formatCurrency(12356.1095,'$',2)).toBe('$12,356.11')
-    // expect(formatCurrency(12356.1005,'$',2)).toBe('$12,356.10')
+    expect(formatCurrency(12356.1005,'$',2)).toBe('$12,356.10')
   })
+  // test('测试保留两位小数-负数', () => {
+  //   expect(formatCurrency(-12356.12454,'$',2)).toBe('-$12,356.12')
+  //   expect(formatCurrency(-12356.105,'$',2)).toBe('-$12,356.11')
+  //   expect(formatCurrency(-12356.1095,'$',2)).toBe('-$12,356.11')
+  //   expect(formatCurrency(-12356.1005,'$',2)).toBe('-$12,356.10')
+  // })
 
 
 })
--- src/__tests__/getRemovedQueryStringInUrl.spec.js
+++ src/__tests__/getRemovedQueryStringInUrl.spec.js
@@ -4,6 +4,7 @@ describe('getRemovedQueryStringInUrl 方法测试', () => {
 
   test('有&的情况', () => {
     expect(getRemovedQueryStringInUrl('user','http://127.0.0.1:8080/?a=sdds&token=t&users=787&user=8989')).toBe('http://127.0.0.1:8080/?a=sdds&token=t&users=787')
+    expect(getRemovedQueryStringInUrl('user','http://127.0.0.1:8080/?a=sdds&token=t&users=787&user=8989#end')).toBe('http://127.0.0.1:8080/?a=sdds&token=t&users=787#end')
     expect(getRemovedQueryStringInUrl('a','http://127.0.0.1:8080/?a=sdds&token=t&user=787')).toBe('http://127.0.0.1:8080/?token=t&user=787')
     expect(getRemovedQueryStringInUrl('ad','http://127.0.0.1:8080/?name=lll&ad=sdds&token=t&user=787')).toBe('http://127.0.0.1:8080/?name=lll&token=t&user=787')
     expect(getRemovedQueryStringInUrl('a','http://127.0.0.1:8080/?name=lll&token=t&user=787&a=sdds')).toBe('http://127.0.0.1:8080/?name=lll&token=t&user=787')
